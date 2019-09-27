5bd0626a72f2c8ec737f0246d760cf7ad6602160
elijahmm17 <elijahmm17@gmail.com>
2019-09-27 20:51:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Code your solutions in this file
 
+function writeCards(names, event) {
+  results = [];
+  for (let i = 0; i < names.length; i++) {
+    console.log("Thank you, ${name}, for the wonderful ${event} gift!");
+  }
+}
